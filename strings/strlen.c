9ebb4cd416cee605531849a82c50b7ad083501f0
Saurabh bharti <100791713+saurabh8github@users.noreply.github.com>
2022-09-15 08:37:28 +0530

    Update strlen.c

--- strings/strlen.c
+++ strings/strlen.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-		char s[400];
-		printf("Enter a string:");
-		gets(s);
+		char s[400]; //String declared 
+		printf("Enter a string:");//taking input 
+		gets(s); 
 		printf("the length of string is %d",strlen(s));
 		return 0;
 }
